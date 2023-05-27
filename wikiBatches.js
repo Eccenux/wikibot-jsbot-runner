f7a9c4fae492cd550520e0679bb69ea7731daa11
Nux <eccenux+git@gmail.com>
2023-05-27 10:21:02 +0200

    extract batch bot

--- wikiBatches.js
+++ wikiBatches.js
@@ -7,12 +7,18 @@ import {
 	wsUrl,
 } from './chrome.config.js'
 
+export default class WikiBatches {
+	constructor(searchUrlTpl, expectedSummary) {
+		this.searchUrlTpl = searchUrlTpl;
+		this.summary = expectedSummary;
+	}
+
 /** Run single page. */
-async function runBatch(browser, batchSize, batchIndex) {
+async runBatch(browser, batchSize, batchIndex) {
 	const offset = batchIndex * batchSize;
 	console.log('runBatch: ', {batchIndex, batchSize, offset});
-	const searchUrl = `https://pl.wikipedia.org/w/index.php?limit=${batchSize}&offset=${offset}&sort=last_edit_asc&search=insource%3A%27imdb.com%2Fname%27+insource%3A%2F%5C%5Bhttps%3F%3A%5C%2F%5C%2Fwww%5C.imdb%5C.com%5C%2Fname%5C%2Fnm%5B0-9a-z%5D%2B%5C%2F+%2F&title=Specjalna:Szukaj&profile=advanced&fulltext=1&ns0=1&ns6=1&ns8=1&ns10=1&ns14=1&ns100=1`;
-	const expectedSummary = 'imdb';
+	const searchUrl = this.searchUrlTpl(batchSize, offset);
+	const expectedSummary = this.summary;
 	const bot = new WikiBot(searchUrl, expectedSummary);
 
 	// new tab for search page
@@ -64,14 +70,15 @@ async function runBatch(browser, batchSize, batchIndex) {
 	return failedPages;
 }
 
-(async () => {
+/** Run all. */
+async runBatches(batches, batchSize) {
 	// connect to current (open) Chrome window
 	const browser = await puppeteer.connect({
 		browserWSEndpoint: wsUrl,
 	});
 	
-	const batches = 1;
-	const batchSize = 10;
+	// const batches = 1;
+	// const batchSize = 10;
 	let total = batchSize * batches;
 	let failedTotal = [];
 	console.log(`Edit ${total} in ${batches} batches.`);
@@ -89,8 +96,6 @@ async function runBatch(browser, batchSize, batchIndex) {
 	}
 	console.log(`done (${total-failedTotal.length}/${total})`);
 	process.exit(0);
+}
 
-})().catch(err => {
-	console.error(err);
-	process.exit(1);
-});
+}
