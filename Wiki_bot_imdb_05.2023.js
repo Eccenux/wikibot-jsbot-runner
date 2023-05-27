85f3510f8bc530062ab729502d3173de2d9a6cbf
Nux <eccenux+git@gmail.com>
2023-05-27 02:32:29 +0200

    large batch

--- Wiki_bot_imdb_05.2023.js
+++ Wiki_bot_imdb_05.2023.js
@@ -14,7 +14,8 @@ import {
 		browserWSEndpoint: wsUrl,
 	});
 
-	const searchUrl = 'https://pl.wikipedia.org/w/index.php?sort=last_edit_asc&search=insource%3A%27imdb.com%2Fname%27+insource%3A%2F%5C%5Bhttps%3F%3A%5C%2F%5C%2Fwww%5C.imdb%5C.com%5C%2Fname%5C%2Fnm%5B0-9a-z%5D%2B%5C%2F+%2F&title=Specjalna:Szukaj&profile=advanced&fulltext=1&ns0=1&ns6=1&ns8=1&ns10=1&ns14=1&ns100=1';
+	const batchSize = 50;
+	const searchUrl = `https://pl.wikipedia.org/w/index.php?limit=${batchSize}&sort=last_edit_asc&search=insource%3A%27imdb.com%2Fname%27+insource%3A%2F%5C%5Bhttps%3F%3A%5C%2F%5C%2Fwww%5C.imdb%5C.com%5C%2Fname%5C%2Fnm%5B0-9a-z%5D%2B%5C%2F+%2F&title=Specjalna:Szukaj&profile=advanced&fulltext=1&ns0=1&ns6=1&ns8=1&ns10=1&ns14=1&ns100=1`;
 	const expectedSummary = 'imdb';
 	const bot = new WikiBot(searchUrl, expectedSummary);
 
@@ -31,7 +32,7 @@ import {
 		let title = await page.title();
 		let url = await page.url();
 		await bot.initViewport(page);
-		
+
 		// Catch edit errors and skip them (e.g. when summary is empty we don't really care).
 		let ok = true;
 		await (async () => {
@@ -51,7 +52,7 @@ import {
 	}
 
 	// loop-edit
-	let max = 20; // whole page
+	let max = batchSize; // whole page
 	for (let index = 0; index < max; index++) {
 		await editPage();
 	}
