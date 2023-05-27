400b8333f3dde7f79bdc56766906eaa134579dfd
Nux <eccenux+git@gmail.com>
2023-05-27 02:58:57 +0200

    run many search pages

--- Wiki_bot_imdb_05.2023.js
+++ Wiki_bot_imdb_05.2023.js
@@ -7,15 +7,11 @@ import {
 	wsUrl,
 } from './chrome.config.js'
 
-
-(async () => {
-	// connect to current (open) Chrome window
-	const browser = await puppeteer.connect({
-		browserWSEndpoint: wsUrl,
-	});
-
-	const batchSize = 50;
-	const searchUrl = `https://pl.wikipedia.org/w/index.php?limit=${batchSize}&sort=last_edit_asc&search=insource%3A%27imdb.com%2Fname%27+insource%3A%2F%5C%5Bhttps%3F%3A%5C%2F%5C%2Fwww%5C.imdb%5C.com%5C%2Fname%5C%2Fnm%5B0-9a-z%5D%2B%5C%2F+%2F&title=Specjalna:Szukaj&profile=advanced&fulltext=1&ns0=1&ns6=1&ns8=1&ns10=1&ns14=1&ns100=1`;
+/** Run single page. */
+async function runBatch(browser, batchSize, batchIndex) {
+	const offset = batchIndex * batchSize;
+	console.log('runBatch: ', {batchIndex, batchSize, offset});
+	const searchUrl = `https://pl.wikipedia.org/w/index.php?limit=${batchSize}&offset=${offset}&sort=last_edit_asc&search=insource%3A%27imdb.com%2Fname%27+insource%3A%2F%5C%5Bhttps%3F%3A%5C%2F%5C%2Fwww%5C.imdb%5C.com%5C%2Fname%5C%2Fnm%5B0-9a-z%5D%2B%5C%2F+%2F&title=Specjalna:Szukaj&profile=advanced&fulltext=1&ns0=1&ns6=1&ns8=1&ns10=1&ns14=1&ns100=1`;
 	const expectedSummary = 'imdb';
 	const bot = new WikiBot(searchUrl, expectedSummary);
 
@@ -56,6 +52,23 @@ import {
 	for (let index = 0; index < max; index++) {
 		await editPage();
 	}
+}
+
+(async () => {
+	// connect to current (open) Chrome window
+	const browser = await puppeteer.connect({
+		browserWSEndpoint: wsUrl,
+	});
+	
+	const batches = 2;
+	const batchSize = 3;
+	for (let batchIndex = batches - 1; batchIndex >= 0; batchIndex--) {
+		await runBatch(browser, batchSize, batchIndex);
+	}
+	
+	// done
+	console.log('done');
+	process.exit(0);
 
 })().catch(err => {
 	console.error(err);
