7321d098952f3aa15fb812f51e6774b466fc9383
Nux <eccenux+git@gmail.com>
2023-06-09 23:29:17 +0200

    soundtrack mock

--- Wiki_bot_imdb_05.2023.js
+++ Wiki_bot_imdb_05.2023.js
@@ -8,19 +8,21 @@ const searchUrlTpl = function(limit, offset) {
 	// const query = [`insource:'imdb.com/name'`, /\[https?:\/\/www\.imdb\.com\/name\/nm[0-9]+\/awards/];
 	
 	// title - awards
-	const query = [ `insource:'imdb.com/title'`, `insource:'/awards'`, /\[https?:\/\/www\.imdb\.com\/title\/tt[0-9]+\/awards/ ];
+	// const query = [ `insource:'imdb.com/title'`, `insource:'/awards'`, /\[https?:\/\/www\.imdb\.com\/title\/tt[0-9]+\/awards/ ];
+	// soundtrack
+	const query = [ `insource:'imdb.com/title'`, `insource:'/soundtrack'`, /\[https?:\/\/www\.imdb\.com\/title\/tt[0-9]+\/soundtrack/ ];
 
 	return batchBot.searchUrl(query, limit, offset);
 }
 const expectedSummary = 'imdb';
 const batchBot = new WikiBatches(searchUrlTpl, expectedSummary);
 // mock
-// batchBot.mock = true;
-// batchBot.mockSleep = 2_000;
+batchBot.mock = true;
+batchBot.mockSleep = 2_000;
 
 (async () => {
-	const batches = 2;
-	const batchSize = 50;
+	const batches = 4;
+	const batchSize = 10;
 	await batchBot.runBatches(batches, batchSize);
 })().catch(err => {
 	console.error(err);
