0fe21b182b60d34ff43c1c88f1466c81ba19c0d8
Nux <eccenux+git@gmail.com>
2023-06-10 19:13:09 +0200

    col-x

---
+++ Wiki_bot_col_06.2023.js
@@ -0,0 +1,23 @@
+import WikiBatches from './wikiBatches.js';
+
+const searchUrlTpl = function(limit, offset) {
+	const query = [ /\{\{col-(break|2)\}\}.{1,3}\*/ ];
+
+	return batchBot.searchUrl(query, limit, offset);
+}
+const expectedSummary = 'WP:Dostępność';
+const batchBot = new WikiBatches(searchUrlTpl, expectedSummary);
+// mock
+// batchBot.mock = true;
+// batchBot.mockSleep = 2_000;
+
+(async () => {
+	const batches = 42;
+	const batchSize = 30;
+	// const batches = 1;
+	// const batchSize = 3;
+	await batchBot.runBatches(batches, batchSize);
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
