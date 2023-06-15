8d5848ea147595ac520a658226c600f9cb2a6067
Nux <eccenux+git@gmail.com>
2023-06-16 00:03:31 +0200

    interwiki part1

---
+++ Wiki_bot_interwiki_06.2023.js
@@ -0,0 +1,27 @@
+import WikiBatches from './wikiBatches.js';
+
+const searchUrlTpl = function(limit, offset) {
+	const query = [
+		`insource:'[[de:'`,
+		/\[\[Kategoria:[^\]]+\]\].{1,5}\[\[de:/,
+	 ];
+
+	return batchBot.searchUrl(query, limit, offset);
+}
+const expectedSummary = 'interwiki';
+const batchBot = new WikiBatches(searchUrlTpl, expectedSummary);
+batchBot.ns = [0];	// main
+// mock
+// batchBot.mock = true;
+// batchBot.mockSleep = 2_000;
+
+(async () => {
+	const batches = 16;
+	const batchSize = 100;
+	// const batches = 1;
+	// const batchSize = 3;
+	await batchBot.runBatches(batches, batchSize);
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
