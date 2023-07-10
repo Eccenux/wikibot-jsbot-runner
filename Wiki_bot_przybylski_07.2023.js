5bb4324258b11df01ed9d69ebd8905f6485939a8
eccenux <eccenux+git@gmail.com>
2023-07-10 23:07:55 +0200

    spacje

--- Wiki_bot_przybylski_07.2023.js
+++ Wiki_bot_przybylski_07.2023.js
@@ -2,7 +2,7 @@ import WikiBatches from './wikiBatches.js';
 
 const searchUrlTpl = function(limit, offset) {
 	// const query = [ `insource:"Adam Przybylski"`, /\[\[Adam Przybylski(\||\]\]| \(1896–1945\)\]\])/ ];
-	const query = [ `insource:"Adam Przybylski"`, /autor link = Adam Przybylski *\|/ ];
+	const query = [ `insource:"Adam Przybylski"`, /autor link *= *Adam Przybylski *\|/ ];
 
 	return batchBot.searchUrl(query, limit, offset);
 }
@@ -14,7 +14,7 @@ const batchBot = new WikiBatches(searchUrlTpl, expectedSummary);
 
 (async () => {
 	const batches = 10;
-	const batchSize = 20;
+	const batchSize = 14;
 	await batchBot.runBatches(batches, batchSize);
 })().catch(err => {
 	console.error(err);
