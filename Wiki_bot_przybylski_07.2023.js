aa1a9cd226e7316ef702f67ffb1adfcc8e697ef9
eccenux <eccenux+git@gmail.com>
2023-07-10 22:36:02 +0200

    link fix

---
+++ Wiki_bot_przybylski_07.2023.js
@@ -0,0 +1,22 @@
+import WikiBatches from './wikiBatches.js';
+
+const searchUrlTpl = function(limit, offset) {
+	// const query = [ `insource:"Adam Przybylski"`, /\[\[Adam Przybylski(\||\]\]| \(1896–1945\)\]\])/ ];
+	const query = [ `insource:"Adam Przybylski"`, /autor link = Adam Przybylski *\|/ ];
+
+	return batchBot.searchUrl(query, limit, offset);
+}
+const expectedSummary = 'Przybylski';
+const batchBot = new WikiBatches(searchUrlTpl, expectedSummary);
+// mock
+// batchBot.mock = true;
+// batchBot.mockSleep = 2_000;
+
+(async () => {
+	const batches = 10;
+	const batchSize = 20;
+	await batchBot.runBatches(batches, batchSize);
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
